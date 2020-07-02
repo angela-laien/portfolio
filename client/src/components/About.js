eb91945c2f2ecaae0f3278ca960f6206bef698e4
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-07-01 21:42:33 -0700

    added skills

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,6 +5,7 @@ const cooking = require('../img/kitchen.png');
 const photography = require('../img/photo.png');
 const travel = require('../img/passport.png');
 const dance = require('../img/fun.png');
+const resume = require('../img/headhunting.png');
 
 function About () {
 
@@ -13,7 +14,17 @@ function About () {
             <div className='newBackground'> </div>
             <div className='int'>
                 <p className='summary'>A software engineering graduate who is seeking to find the opportunity to work in a fun and challenging working environment that will encourage her to improve and learn new and neccessary skills as well as be motivated by the company to do her best for the sake of helping herself and the company advancement in the software engineering industry.</p>
-                <h2 className='interest'>Interest</h2>
+                <div className='cv'>
+                    <h2 className='cv-title'>CV</h2>
+                    <a
+                        href='https://drive.google.com/file/d/1c1B8Rovu9bt1aldkvORV944dsH3eThW2/view?usp=sharing'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <img className='resume' src={resume} alt='resume logo'/>
+                    </a>
+                </div>
+                <h3 className='interest'>Interest</h3>
                 <div className='section'>
                     <img className='icons' src={photography} alt='camera logo'/>
                     <img className='icons' src={yoga} alt='yoggi logo'/>
--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
-export default () => (
-    <div className='newBackground'>
+const css3 = require('../img/css.png');
+const html5 = require('../img/html5.png');
+const javascript = require('../img/javascript.png');
+const nodejs = require('../img/nodejs.png');
+const python = require('../img/python-file.png');
+const reactjs = require('../img/react.png');
+const sql = require('../img/sql.png');
+const ux = require('../img/ux.png');
 
-    </div>
-)
+function Skills() {
+
+    return(
+        <div className='skills'>
+            <div className='newBackground'> </div>
+            <div className='skill-sec'>
+                <img className='logo' src={html5} alt='html5 icon'/>
+                <img className='logo' src={css3} alt='css3 icon'/>
+                <img className='logo' src={python} alt='python icon'/>
+                <img className='logo' src={javascript} alt='js icon'/>
+            </div>
+            <div className='skill-sec'>
+                <img className='logo' src={reactjs} alt='react.js icon'/>
+                <img className='logo' src={sql} alt='sql icon'/>
+                <img className='logo' src={nodejs} alt='node.js icon'/>
+                <img className='logo' src={ux} alt='ux icon'/>
+            </div>
+        </div>
+    )
+}
+
+export default Skills;
