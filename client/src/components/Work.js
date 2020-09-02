11cf11d3365df89eeb01056c2d5ee6b60dafb803
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-09-02 12:56:01 -0700

    added a work piece

--- client/src/components/Work.js
+++ client/src/components/Work.js
@@ -1,48 +1,42 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 
 const freeStyleKitchen = require('../img/FreeStyleKitchen.png');
+const gameOfLife = require('../img/GameOfLife.png');
 
 export default () => (
     <Carousel showThumbs={false} showStatus={true}>
-        <div className='project'>
-            <a 
-                href='https://www.freestylekitchen.club/' 
-            >
-                <img className='image' 
-                src={freeStyleKitchen} 
-                alt='freeStyleKitchen' 
-                style={{
-                    padding: '0% 10% 0% 0%',
-                    textAlign: 'center'
-                }}
-                />
-            </a>
-            <p className='kitchen'
-                style={{ 
-                color: 'white', 
-                lineHeight: 2,
-                padding: '0% 10% 0% 0%',
-                textAlign: 'center'
-                }}
-                >
-                <a 
-                    style={{ 
-                    color: 'white' }}
-                    href='https://github.com/angela-laien/CookWithLove-fe' 
-                >
-                    Freestyle Kitchen Frontend
+        <div>
+            <img src={freeStyleKitchen} alt='freeStyleKitchen'/>
+            <p className='project'>
+                <a href='https://www.freestylekitchen.club/'>
+                    Freestyle Kitchen
                 </a>
-                <br />
-                <a 
-                    style={{ 
-                    color: 'white' }}
-                    href='https://github.com/angela-laien/InstaCook' 
-                >
-                    Freestyle Kitchen Backend
+                : A cookbook project featuring creative recipes. 
+                <br/>Utilized: Node, React, SQL, AWS Amplify, HTML5, CSS3
+                <br/>
+                <a href='https://github.com/angela-laien/CookWithLove-fe'>
+                    Front End Repo
                 </a>
-                <br />
-                A personal cookbook project featuring my weekend recipes. <br />Utilized: Node, React, SQL, AWS Amplify, HTML5, CSS3
+                <br/>
+                <a href='https://github.com/angela-laien/InstaCook'>
+                    Back End Repo
+                </a>
+            </p>
+        </div>
+        <div>
+            <img src={gameOfLife} alt='gameofLife'/>
+            <p className='project'>
+                <a href='https://game-of-life-sable.vercel.app/'>
+                    Game of life
+                </a>
+                : A vintage cellular automation zero player game from the 70s. 
+                <br/>Utilized: React, HTML5, CSS3
+                <br/>
+                <a href='https://github.com/angela-laien/Game_of_Life'>
+                    GitHub Repo
+                </a>
             </p>
         </div>
     </Carousel>
